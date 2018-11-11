30640b0cc6230bced06e152fea041c2fda1fa4f3
shki96 <44605606+shki96@users.noreply.github.com>
2018-11-11 18:22:18 +0900

    ㅗㅑ
    ㅗㅑ

---
+++ src/Gui/test.java
@@ -0,0 +1,34 @@
+package tttttt;
+
+import java.awt.Container;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class test extends JFrame {
+
+	
+	
+	public test(String x){
+		super("±è»óÈÆ ¤´");
+		init();
+		first();
+	}
+	
+	public void init() {
+		setBounds(100,100,300,200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);	
+	}
+	
+	public void first() {
+		Container cont = getContentPane();
+		JLabel jl = new JLabel("Àú´Â ±è»óÈÆÀÔ´Ï´Ù.");
+		cont.
+		
+		cont.add(jl);
+	}
+	public static void main(String[] args) {
+		
+	}
+}
