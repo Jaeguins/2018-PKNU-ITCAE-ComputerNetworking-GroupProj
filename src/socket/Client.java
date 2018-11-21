0c32f9d253850b46175a1637e4ed96d3c40b02a7
wnalsals123 <wnalsals12@naver.com>
2018-11-21 18:40:16 +0900

    자바 소켓 프로그래밍 이해

--- src/socket/Client.java
+++ src/socket/Client.java
@@ -1,7 +1,25 @@
 package socket;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
 public class Client {
-	public static void main(String arg[]){
-        System.out.println("Mine Networking Start!!!");
+	public static void main(String[] args)
+    {
+        try{
+            Socket mc_socket = new Socket("127.0.0.1", 8888);
+
+            InputStream input_data = mc_socket.getInputStream();
+
+            byte[] recstr = new byte[100];
+            input_data.read(recstr);
+
+            System.out.println(new String(recstr));
+
+            mc_socket.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/socket/Server.java
+++ src/socket/Server.java
@@ -1,4 +1,26 @@
 package socket;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+	public static void main(String[] args)
+    {
+        try{
+            ServerSocket ms_socket = new ServerSocket(8888);
+            Socket mc_socket = ms_socket.accept();
+
+            OutputStream output_data = mc_socket.getOutputStream();
+
+            String sendstr = "Welcome to Mine";
+            output_data.write(sendstr.getBytes());
+
+            ms_socket.close();
+            mc_socket.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
