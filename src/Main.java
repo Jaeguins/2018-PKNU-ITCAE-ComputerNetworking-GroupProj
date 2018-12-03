7feef599b8a95a8debcc3449514373d161f9bbc1
Jaeguins <jsu4838@naver.com>
2018-12-03 18:45:28 +0900

    Main class add

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import game.Game;
+import gui.CustFrame;
+import socket.Client;
+import socket.Server;
+
+import javax.swing.*;
+
+public class Main {
+    Server server;
+    Client client;
+    Game game;
+    CustFrame frame;
+    public Main(){
+        
+    }
+    public static void main(String args[]){
+        Main m=new Main();
+    }
+}
---
+++ src/gui/CustFrame.java
@@ -0,0 +1,35 @@
+package gui;
+
+import java.awt.Container;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CustFrame extends JFrame {
+
+	
+	public CustFrame(){
+		super("Áö·ÚÃ£±â");
+		init();
+		first();
+	}
+	
+	public void init() {
+		setBounds(100,100,300,200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);	
+	}
+	
+	public void first() {
+		setLayout(null);
+		Container cont = getContentPane();
+		JPanel jp= new InitialPanel(cont);
+		cont.add(jp);
+		
+		
+	}
+	public static void main(String[] args) {
+		CustFrame ts = new CustFrame();
+	}
+}
+
